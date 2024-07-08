bff8f9a6e01138d27bc0eb3ce0fa060f22f44716
Aniket tyagi <121341144+anikett001@users.noreply.github.com>
2024-07-08 17:31:03 +0530

    Update DSA-Mocktests.js

--- DSA-Mocktests.js
+++ DSA-Mocktests.js
@@ -9,3 +9,29 @@ function findMedian(arr) {
  }
  return strArr[mid]
 }
+
+
+
+Q-2 Flipping the Matrix 
+
+
+function flippingMatrix(matrix) {
+ let max = 0;
+ let n = matrix.length/2;
+ let total = 0;
+ for(let row = 0; row < n; row++){
+ for(let col = 0; col < n; col++){
+ max = Number.MIN_VALUE
+ max = Math.max(matrix[row][col],max); // upper left [0,0]
+ max = Math.max(matrix[row][2 * n - col -1],max); // upper right
+[0,1]
+ max = Math.max(matrix[2 * n -row -1][col],max); // lower left
+[1,0]
+ max = Math.max(matrix[2 * n - row -1][2 * n - col -1],max); //
+lower right [1,1]
+ 
+ total += max
+ }
+ }
+ return total
+}
